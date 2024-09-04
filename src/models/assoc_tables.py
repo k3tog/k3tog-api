6e939bd002fa887dd3e06d6806efc9ad5377b735
Irene <mijeong.irene.ban@gmail.com>
2024-09-03 19:22:45 -0700

    fix

--- src/models/assoc_tables.py
+++ src/models/assoc_tables.py
@@ -1,8 +1,11 @@
 import logging
-import os
 
 from sqlalchemy import BigInteger, Column, DateTime, ForeignKey, Table, func, Float
-from db.database import DEFAULT_DB_SCHEMA_NAME, Base
+from db.database import Base
+from models.project import Project
+from models.user_gauge import UserGauge
+from models.user_needle import UserNeedle
+from models.user_yarn import UserYarn
 
 
 logger = logging.getLogger(__name__)
@@ -10,8 +13,8 @@ logger = logging.getLogger(__name__)
 project_needle = Table(
     "project_needle",
     Base.metadata,
-    Column("project_id", BigInteger, ForeignKey("project.id"), primary_key=True),
-    Column("needle_id", BigInteger, ForeignKey("user_needle.id"), primary_key=True),
+    Column("project_id", BigInteger, ForeignKey(Project.id), primary_key=True),
+    Column("needle_id", BigInteger, ForeignKey(UserNeedle.id), primary_key=True),
     Column("created_ts", DateTime, server_default=func.now()),
     Column("updated_ts", DateTime, onupdate=func.now()),
 )
@@ -19,8 +22,8 @@ project_needle = Table(
 project_yarn = Table(
     "project_yarn",
     Base.metadata,
-    Column("project_id", BigInteger, ForeignKey("project.id"), primary_key=True),
-    Column("yarn_id", BigInteger, ForeignKey("user_yarn.id"), primary_key=True),
+    Column("project_id", BigInteger, ForeignKey(Project.id), primary_key=True),
+    Column("yarn_id", BigInteger, ForeignKey(UserYarn.id), primary_key=True),
     Column("created_ts", DateTime, server_default=func.now()),
     Column("updated_ts", DateTime, onupdate=func.now()),
     Column("num_used", Float),
@@ -29,8 +32,8 @@ project_yarn = Table(
 project_gauge = Table(
     "project_gauge",
     Base.metadata,
-    Column("project_id", BigInteger, ForeignKey("project.id"), primary_key=True),
-    Column("gauge_id", BigInteger, ForeignKey("user_gauge.id"), primary_key=True),
+    Column("project_id", BigInteger, ForeignKey(Project.id), primary_key=True),
+    Column("gauge_id", BigInteger, ForeignKey(UserGauge.id), primary_key=True),
     Column("created_ts", DateTime, server_default=func.now()),
     Column("updated_ts", DateTime, onupdate=func.now()),
 )
