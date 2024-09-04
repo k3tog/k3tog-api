ef2cf5165b4b56989fd0ee3255c5d1452ee8a191
Irene <mijeong.irene.ban@gmail.com>
2024-09-03 19:29:34 -0700

    circular import issue fix

--- src/models/assoc_tables.py
+++ src/models/assoc_tables.py
@@ -3,20 +3,20 @@ import os
 
 from sqlalchemy import BigInteger, Column, DateTime, ForeignKey, Table, func, Float
 from db.database import DEFAULT_DB_SCHEMA_NAME, Base
-from models.user_gauge import UserGauge
-from models.user_needle import UserNeedle
-from models.user_yarn import UserYarn
 
 
 logger = logging.getLogger(__name__)
 
 project_id = f"{os.environ.get('DB_SCHEMA', DEFAULT_DB_SCHEMA_NAME)}.project.id"
+user_needle_id = f"{os.environ.get('DB_SCHEMA', DEFAULT_DB_SCHEMA_NAME)}.user_needle.id"
+user_yarn_id = f"{os.environ.get('DB_SCHEMA', DEFAULT_DB_SCHEMA_NAME)}.user_yarn.id"
+user_gauge_id = f"{os.environ.get('DB_SCHEMA', DEFAULT_DB_SCHEMA_NAME)}.user_gauge.id"
 
 project_needle = Table(
     "project_needle",
     Base.metadata,
     Column("project_id", BigInteger, ForeignKey(project_id), primary_key=True),
-    Column("needle_id", BigInteger, ForeignKey(UserNeedle.id), primary_key=True),
+    Column("needle_id", BigInteger, ForeignKey(user_needle_id), primary_key=True),
     Column("created_ts", DateTime, server_default=func.now()),
     Column("updated_ts", DateTime, onupdate=func.now()),
 )
@@ -25,7 +25,7 @@ project_yarn = Table(
     "project_yarn",
     Base.metadata,
     Column("project_id", BigInteger, ForeignKey(project_id), primary_key=True),
-    Column("yarn_id", BigInteger, ForeignKey(UserYarn.id), primary_key=True),
+    Column("yarn_id", BigInteger, ForeignKey(user_yarn_id), primary_key=True),
     Column("created_ts", DateTime, server_default=func.now()),
     Column("updated_ts", DateTime, onupdate=func.now()),
     Column("num_used", Float),
@@ -35,7 +35,7 @@ project_gauge = Table(
     "project_gauge",
     Base.metadata,
     Column("project_id", BigInteger, ForeignKey(project_id), primary_key=True),
-    Column("gauge_id", BigInteger, ForeignKey(UserGauge.id), primary_key=True),
+    Column("gauge_id", BigInteger, ForeignKey(user_gauge_id), primary_key=True),
     Column("created_ts", DateTime, server_default=func.now()),
     Column("updated_ts", DateTime, onupdate=func.now()),
 )
